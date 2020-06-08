55e7b241e31fa8d5d9caa2618f4266c604b8ff2a
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-08 17:05:33 -0400

    Update first_run.sh
    Added keys

--- first_run.sh
+++ first_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
+sudo echo "deb-src http://vontaene.de/apt/ . main" >> /etc/apt/sources.list
+sudo gpg --recv-key 0C667A3E && gpg -armor --export 0C667A3E | apt-key add -
 sudo apt update && sudo apt dist-upgrade -y && sudo apt install gstreamer1.0 python3-gpiozero -y
 pip3 install playsound
 mkdir /home/pi/sounds
