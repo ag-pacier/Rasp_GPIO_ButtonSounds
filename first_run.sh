9b7dfc727f94e453f539b2e68d3c5a27b2832d79
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-08 00:35:57 -0400

    Update first_run.sh
    Adding other dependancies and add a bit more documentation

--- first_run.sh
+++ first_run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-gpiozero -y
+sudo echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
+sudo apt update && sudo apt dist-upgrade -y && sudo apt install gstreamer1.0 python3-gpiozero -y
+pip3 install playsound
+mkdir /home/pi/sounds
 echo "Check documentation at https://gpiozero.readthedocs.io/en/stable/recipes.html"
+echo "Place MP3s or WAVs in /home/pi/sounds"
