6b76739b17f06fcb89f22275a8cc0a41b0d562e7
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-07 23:15:32 -0400

    Create first_run.sh
    Created first run script for Raspberry Pi and a reminder of the documentation

---
+++ first_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-gpiozero -y
+echo "Check documentation at https://gpiozero.readthedocs.io/en/stable/recipes.html"
