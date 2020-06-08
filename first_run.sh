3ebe8b3c2e4b7354fb6df81be1a0304d977db5dd
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-08 17:12:07 -0400

    Update first_run.sh
    Fixed the bunk

--- first_run.sh
+++ first_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
-sudo echo "deb-src http://vontaene.de/apt/ . main" >> /etc/apt/sources.list
-sudo gpg --recv-key 0C667A3E && gpg -armor --export 0C667A3E | apt-key add -
-sudo apt update && sudo apt dist-upgrade -y && sudo apt install gstreamer1.0 python3-gpiozero -y
+sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-pip gstreamer1.0-tools python3-gpiozero -y
 pip3 install playsound
 mkdir /home/pi/sounds
 echo "Check documentation at https://gpiozero.readthedocs.io/en/stable/recipes.html"
