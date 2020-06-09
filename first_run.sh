5aa1056ad51e1c36e006bbcb5578cbb81d82d270
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-09 10:11:22 -0400

    Update first_run.sh
    Changed package requirements to use omxplayer

--- first_run.sh
+++ first_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-pip gstreamer1.0-tools python3-gpiozero -y
-pip3 install playsound
+sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-pip omxplayer python3-gpiozero -y
 mkdir /home/pi/sounds
 echo "Check documentation at https://gpiozero.readthedocs.io/en/stable/recipes.html"
 echo "Place MP3s or WAVs in /home/pi/sounds"
