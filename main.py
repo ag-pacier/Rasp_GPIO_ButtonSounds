65e187a285ea58599c44b49f408cd483d18458b5
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-09 10:11:50 -0400

    Update main.py
    Switched to omxplayer and stopped checking validity for now. Rechecks directory everytime the button is pushed to allow on-the-fly changes

--- main.py
+++ main.py
@@ -2,37 +2,35 @@
 #Main loop for button
 
 from gpiozero import Button
-from playsound import playsound
 from os import listdir
+from os.path import join
 from sys import exit
 from random import choice
 from signal import pause
+from subprocess import run
 
 big_button = Button(21)
 sound_loc = '/home/pi/sounds/'
-valid_types = ('mp3', 'wav', 'MP3', 'WAV')
-sounds = []
 
 def find_sounds():
     ''' Hunt through sound_loc and list out all the valid sounds in the sounds list'''
-    global sounds
-    files = listdir(sound_loc)
-    for file in files:
-        if file[-3:] in valid_types:
-            sounds.append(file)
+    sounds = []
+    for file in listdir(sound_loc):
+        sounds.append(join(sound_loc, file)
     if len(sounds) <= 0:
         exit("No sounds located at " + sound_loc)
+    else:
+        return sounds
 
 
 def play_sound():
     '''randomly pick a sound from the valid sounds list and play it'''
-    sound = choice(sounds)
-    playsound(sound_loc + sound)
+    sound = choice(find_sounds())
+    run('omxplayer', sound)
     print("Played: " + sound)
 
 
 if __name__ == "__main__":
     #If run, make the list of sounds and play a sound every time the button is pressed
-    find_sounds
     big_button.when_pressed = play_sound
     pause()
