880b07034caa46b8d7dddb01fe083a4764d9066b
Gage Pacier <48218237+dietolead@users.noreply.github.com>
2020-06-09 10:26:28 -0400

    Update first_run.sh
    Added service file enabling to the first run script

--- first_run.sh
+++ first_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt dist-upgrade -y && sudo apt install python3-pip omxplayer python3-gpiozero -y
 mkdir /home/pi/sounds
-echo "Check documentation at https://gpiozero.readthedocs.io/en/stable/recipes.html"
-echo "Place MP3s or WAVs in /home/pi/sounds"
+sudo cp ./button_mon.service /etc/systemd/system/button_mon.service
+sudo systemctl daemon-reload
+sudo systemctl enable button_mon.service
+echo "Place MP3s or WAVs in /home/pi/sounds and reboot"
